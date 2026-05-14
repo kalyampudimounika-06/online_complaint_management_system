83ae181da6630941ad53f1bdd0cf37346a8e8eff
Naidu Pavan <naidupavan573@gmail.com>
2026-05-14 17:02:35 +0530

    Fixed forgot password email issue

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -144,13 +144,19 @@ router.post("/forgot-password", async (req, res) => {
         // Email Transport
         const transporter = nodemailer.createTransport({
 
-            service: "gmail",
+            host: "smtp.gmail.com",
+
+            port: 465,
+
+            secure: true,
 
             auth: {
+
                 user: process.env.EMAIL_USER,
+
                 pass: process.env.EMAIL_PASS
             }
-             });
+        });
 
         // Send Mail
         await transporter.sendMail({
